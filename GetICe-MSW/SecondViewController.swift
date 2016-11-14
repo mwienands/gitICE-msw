e588e2b0b23dc3420fb23973c43c20e9e6164587
mwienands <mwienands@michaels-mbp-2.nwmissouri.edu>
2016-11-14 13:24:02 -0600

    Initial Commit

---
+++ GetICe-MSW/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GetICe-MSW
+//
+//  Created by Michael Wienands on 11/14/16.
+//  Copyright © 2016 Michael Wienands. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
