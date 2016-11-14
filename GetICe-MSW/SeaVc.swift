f145358bce6e2fc633d3b00fd3e64d8e39265cc4
mwienands <mwienands@michaels-mbp-2.nwmissouri.edu>
2016-11-14 13:38:49 -0600

    First commit trying to work with file systems. Never done this before/

---
+++ GetICe-MSW/SeaVc.swift
@@ -0,0 +1,13 @@
+//
+//  SeaVc.swift
+//  GetICe-MSW
+//
+//  Created by Michael Wienands on 11/14/16.
+//  Copyright © 2016 Michael Wienands. All rights reserved.
+//
+
+import UIKit
+
+class SeaVc: UIViewController {
+
+}
